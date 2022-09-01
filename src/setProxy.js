4edb79c35190b5138681f684bbcbb711ab3e71ae
Jongmin <come145@naver.com>
2022-09-01 18:54:55 +0900

    프론트 커밋

---
+++ src/setProxy.js
@@ -0,0 +1,15 @@
+/*
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: "http://localhost:8080", //# 서버 URL or localhost:설정한포트번호
+      changeOrigin: true,
+    })
+  );
+};
+
+//프록시란 ? 클라이언트가 다른 네트워크를 간접적으로 접근하게 해주는것
+*/
